34987df2e926d6dd6378f773fdec0f4368d4b617
nohsa97 <81568105+nohsa97@users.noreply.github.com>
2022-07-07 00:59:16 +0900

    dd

--- KotStud3/app/src/main/java/com/example/kotstud3/MainActivity.kt
+++ KotStud3/app/src/main/java/com/example/kotstud3/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.kotstud3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn.setOnClickListener{
+            layoutInflater.inflate(R.layout.part1,container,true)
+        }
     }
 }
