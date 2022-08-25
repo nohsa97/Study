708831cb5a30327618e887a14d9720ba0532d05e
nohsa97 <81568105+nohsa97@users.noreply.github.com>
2022-08-26 03:44:52 +0900

    2805 문제 시간초과
    해결방법생각중

--- CodingTest/src/q2805.java
+++ CodingTest/src/q2805.java
@@ -8,6 +8,8 @@ public class q2805 {
 
     public static void main(String[] args) throws IOException {
         int M,N;
+        int MAX=0;
+        int arrMax=0;
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         String s;
         StringTokenizer st;
@@ -26,9 +28,19 @@ public class q2805 {
         for(int i=0;i<N;i++)
             arr.add(Integer.parseInt(st.nextToken()));
 
-//        int result = get(arr,15);
-//        System.out.println(result);
+        for(int i=0; i<N;i++) {
+            if(arrMax<arr.get(i))
+                arrMax=arr.get(i);
+        }
 
+        for(int i=0 ; i<arrMax;i++){
+            if((M==get(arr,i)) && MAX<get(arr,i)){
+                MAX=i;
+            }
+
+        }
+
+        System.out.println(MAX);
 
     }
 
@@ -38,9 +50,5 @@ public class q2805 {
             if(item-M > 0)
                 result += item-M;
         return result;
-    }
-    public static void arrget(ArrayList<Integer> arr,int M) {
-
-
     }
 }
