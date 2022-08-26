a454fd8e49623c0a1966a8c39d979f844f437d77
nohsa97 <81568105+nohsa97@users.noreply.github.com>
2022-08-27 00:36:18 +0900

    2805 이진탐색 실패

--- CodingTest/src/q2805.java
+++ CodingTest/src/q2805.java
@@ -25,30 +25,47 @@ public class q2805 {
         s = bf.readLine();
         st = new StringTokenizer(s);
 
-        for(int i=0;i<N;i++)
-            arr.add(Integer.parseInt(st.nextToken()));
+
 
         for(int i=0; i<N;i++) {
+            arr.add(Integer.parseInt(st.nextToken()));
             if(arrMax<arr.get(i))
                 arrMax=arr.get(i);
-        }
-
-        for(int i=0 ; i<arrMax;i++){
-            if((M==get(arr,i)) && MAX<get(arr,i)){
-                MAX=i;
-            }
 
         }
+
+//        for(int i=0 ; i<arrMax;i++){
+//            if((M==getResult(arr,i)) && MAX<getResult(arr,i)){
+//                MAX=i;
+//            }
+//        }
+        MAX = getMid(1,arrMax,arr,M);
 
         System.out.println(MAX);
 
     }
 
-    public static int get(ArrayList<Integer> arr,int M) {
+    public static int getResult(ArrayList<Integer> arr,int M) {
         int result=0;
         for(Integer item : arr)
             if(item-M > 0)
                 result += item-M;
         return result;
     }
+
+    public static int getMid(int low,int high,ArrayList<Integer> arr,int M) {
+        int mid = (low+high)/ 2;
+        if(getResult(arr,mid)>M){
+           return getMid(mid+1,high,arr,M);
+        }
+        else if (getResult(arr,mid)<M){
+           return getMid(low,mid,arr,M);
+        }
+        else if(getResult(arr,mid)==M)
+            return mid;
+        return -1;
+    }
 }
+
+// 20 15 10 17
+// 10    10  5 0 7  22 >
